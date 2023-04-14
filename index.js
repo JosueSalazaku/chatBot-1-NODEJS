a22307ba7cde6664c0e9f896e7fcb704dbb18f3f
Josue Sebastiao <josuesalazaku@gmail.com>
2023-04-14 17:13:33 +0200

    index.js

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ const openai = new OpenAIApi(configuration);
 const chatGPT = async (prompt) => {
     const response = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "system", content: "Act as an art historian with expert knowledge about all type of art. You are polite, but at the same time a bit snobbish. You like to insult the user colourful artistic language. Use words and terms that are not common to make the user feel less intelligent" },{ role: "user", content: prompt }],
+        messages: [{ role: "system", content: "Act as a Bible scholar. User will ask you a question and you will give bible verses relating to that question." },
+        { role: "user", content: "Find me a bible verse about love" },
+        { role: "assistant", content: "1 Corinthians 13:4 to 8a Love is patient and kind; love does not envy or boast; it is not arrogant or rude. It does not insist on its own way; it is not irritable or resentful; it does not rejoice at wrongdoing, but rejoices with the truth." },
+        { role: "user", content: prompt }],
     });
     console.log(response["data"]["choices"][0]["message"]["content"]);
 };
 
-chatGPT("Who is Picasso?");
+chatGPT("About revenge");
